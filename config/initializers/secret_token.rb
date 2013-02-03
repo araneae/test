bff9af983ab8f6582e28e7c29f6bd70d710698da
bbaruah <bubulbaruah@gmail.com>
2013-02-02 22:35:56 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spider::Application.config.secret_token = '67e78d7809a81309d75c0bfe30ef5bef960dd8ecf6ae1dd6bf7a71b7d1a61131d44925d52a5155a0f145ee5482e76c7eac9aa61fac557152ac2691136abe4ac7'
