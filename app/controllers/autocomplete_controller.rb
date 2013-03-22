40bc88c696218bea54d958203095472d91779b7f
bbaruah <bubulbaruah@gmail.com>
2013-03-22 09:20:30 -0700

    handled case insensitive search in controller

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -5,7 +5,7 @@ class AutocompleteController < ApplicationController
     if term
        like=term[0].concat("%")
        list = Rails.cache.fetch(like) do
-          addresses=Address.where("address like ?", like)
+          addresses=Address.where("lower(address) like ?", like.downcase)
           list = addresses.map {|item| Hash[id: item.id, name: item.address]}
       end
     end
